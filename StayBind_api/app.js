c4a5f006194f80b0654249db148833e4d9060c80
shoubhik18 <shoubhikpan2015@gmail.com>
2024-07-15 11:13:22 +0530

    vercel.json

--- StayBind_api/app.js
+++ StayBind_api/app.js
@@ -1,37 +1,3 @@
-
-// const express = require('express');
-// const { sequelize } = require('./models'); // Import sequelize instance from models
-// require('dotenv').config();
-// const app = express();
-// const port = process.env.PORT || 5432;
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Routes
-// const authRoutes = require('./routes/auth');
-// const leadRoutes = require('./routes/lead');
-// app.use('/api/auth', authRoutes);
-// app.use('/api/leads', leadRoutes);
-
-// // Test DB connection and sync models
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Database connected...');
-//     return sequelize.sync();
-//   })
-//   .then(() => {
-//     console.log('Models synchronized...');
-//     // Start the server
-//     app.listen(port, () => {
-//       console.log(`Server running on port ${port}`);
-//     });
-//   })
-//   .catch(err => console.error('Error connecting to the database:', err));
-
-
-
 // app.js
 
 // const express = require('express');
